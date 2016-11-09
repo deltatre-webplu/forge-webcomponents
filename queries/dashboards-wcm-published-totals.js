8f656e609a6ceb6fd3727579b3c6d0eb1d55760a
Davide Icardi <davide.icardi@gmail.com>
2016-11-09 20:02:20 +0100

    Fixed queries

--- queries/dashboards-wcm-published-totals.js
+++ queries/dashboards-wcm-published-totals.js
@@ -1,65 +1,84 @@
 var MongoClient = require('mongodb').MongoClient;
 
 class Query extends QueryBase {
-	constructor(config){
+	constructor(config) {
 		super(config);
 	}
 
-	query(options){
+	query(options) {
 		return MongoClient.connect(this._config.backEndStoreConfiguration.ConnectionString)
 		.then((db) => {
-				var col = db.collection('wcm.EntitiesPublished');
+			var col = db.collection('wcm.EntitiesPublished');
 
-				var grandTotal = col.aggregate([{$group:{_id:"$EntityId"}},{$group:{_id:"grandTotal",count:{$sum:1}}}])
-							.toArray();
-				var totalByEntityType = col.aggregate([
-					    {
-					      $group:{_id:{"EntityId":"$EntityId","EntityType":"$EntityType"}}
-					    },{
-					      $group:{_id:"$_id.EntityType",count:{$sum:1}}
-					    }])
-				.toArray();
+			var grandTotal = col.aggregate([{
+				$group: {
+					_id: "$EntityId"
+				}
+			}, {
+				$group: {
+					_id: "grandTotal",
+					count: {
+						$sum: 1
+					}
+				}
+			}])
+			.toArray();
+			var totalByEntityType = col.aggregate([{
+				$group: {
+					_id: {
+						"EntityId": "$EntityId",
+						"EntityType": "$EntityType"
+					}
+				}
+			}, {
+				$group: {
+					_id: "$_id.EntityType",
+					count: {
+						$sum: 1
+					}
+				}
+			}])
+			.toArray();
 
-		    return Promise.all([grandTotal, totalByEntityType])
-				.then((results) => {
-					db.close();
-					return results;
-				});
+			return Promise.all([grandTotal, totalByEntityType])
+			.then((results) => {
+				db.close();
+				return results;
+			});
 		})
 		.then((results) => {
-		    var result = {};
-	    	result.TotalByEntities = [];
+			var result = {};
+			result.TotalByEntities = [];
 
-	    	result.Total = results[0][0].count;
-				results[1].forEach(function (r){
-		        var obj=new Object;
-		        obj.EntityType = r._id;
-		        obj.Total = r.count;
-		        result.TotalByEntities.push(obj);
-					});
+			result.Total = results[0][0].count;
+			results[1].forEach(function(r) {
+				var obj = new Object;
+				obj.EntityType = r._id;
+				obj.Total = r.count;
+				result.TotalByEntities.push(obj);
+			});
 
-				if(options && options.outputType =='charts'){
-					return this.totalsForCharts(result.TotalByEntities);
-				}
+			if (options && options.outputType == 'charts') {
+				return this.totalsForCharts(result.TotalByEntities);
+			}
 
-				return result;
-      }
-		);
+			return result;
+		});
 	}
 
 	totalsForCharts(totals) {
-    if (!totals) return null;
-    var chartotals = [];
-    var header = ["EntityType", "Total"];
-    chartotals.push(header);
+		if (!totals) return null;
+		var chartotals = [];
+		var header = ["EntityType", "Total"];
+		chartotals.push(header);
 
-    totals.forEach(function(t){
-      var byEntity=[];
-      byEntity.push(t.EntityType);
-      byEntity.push(t.Total);
-      chartotals.push(byEntity);
-    });
+		totals.forEach(function(t) {
+			var byEntity = [];
+			byEntity.push(t.EntityType);
+			byEntity.push(t.Total);
+			chartotals.push(byEntity);
+		});
 
-    return chartotals;
-  }
+		return chartotals;
+	}
 }
--- queries/dashboards-wcm-trends.js
+++ queries/dashboards-wcm-trends.js
@@ -1,185 +1,299 @@
 var MongoClient = require('mongodb').MongoClient;
 
 class Query extends QueryBase {
-	constructor(config){
+	constructor(config) {
 		super(config);
 	}
 
-	query(options){
+	query(options) {
 		return MongoClient.connect(this._config.backEndStoreConfiguration.ConnectionString)
-		.then((db) => {
-  			var col = db.collection('wcm.EntitiesPublished');
+			.then((db) => {
+				var col = db.collection('wcm.EntitiesPublished');
 				var results;
 				switch (options.span) {
-					case "day":
-						results = this._lastDay(col);
-						break;
-					case "month":
-						 results = this._lastMonth(col)
-						break;
-					case "semester":
-						results = this._lastSemester(col)
-						break;
-					default:
-						return null;
-						break;
+				case "day":
+					results = this._lastDay(col);
+					break;
+				case "month":
+					results = this._lastMonth(col);
+					break;
+				case "semester":
+					results = this._lastSemester(col);
+					break;
+				default:
+					return null;
 				}
 				return results.then(function(res) {
 					db.close();
 					return res;
 				});
-		});
+			});
 	}
 
-  _lastDay(col){
+	_lastDay(col) {
 
-		var
-
-    var promise = col.aggregate([
-	    {
-				$match: {
-					"PublishedOn":{$gt: new Date(Date.now() - 24*60*60 * 1000)}
+		var promise = col.aggregate([{
+			$match: {
+				"PublishedOn": {
+					$gt: new Date(Date.now() - 24 * 60 * 60 * 1000)
 				}
-			},
-	    {
-				"$project": {
-	      	"EntityId":"$EntityId",
-	        "EntityType":"$EntityType",
-	        "Year": { $year: "$PublishedOn" },
-	        "Day": { $dayOfMonth: "$PublishedOn" },
-	        "Month": { $month: "$PublishedOn" },
-	        "Hour":{$hour:"$PublishedOn"}
-				}
-	    },
-	    {
-				$group: {
-					_id: {"EntityId":"$EntityId","Year":"$Year","Month":"$Month","Day":"$Day","Hour":"$Hour"}
-				}
-			},
-	    {
-				$group: {
-					_id: {"Year":"$_id.Year","Month":"$_id.Month","Day":"$_id.Day","Hour":"$_id.Hour"},
-					"Total": { $sum:1 }
-				}
-	   	},
-	   	{
-				$sort : { "_id.Year" : 1, "_id.Month" : 1, "_id.Day" : 1 , "_id.Hour" : 1 }
 			}
-		]).toArray();
+		}, {
+			"$project": {
+				"EntityId": "$EntityId",
+				"EntityType": "$EntityType",
+				"Year": {
+					$year: "$PublishedOn"
+				},
+				"Day": {
+					$dayOfMonth: "$PublishedOn"
+				},
+				"Month": {
+					$month: "$PublishedOn"
+				},
+				"Hour": {
+					$hour: "$PublishedOn"
+				}
+			}
+		}, {
+			$group: {
+				_id: {
+					"EntityId": "$EntityId",
+					"Year": "$Year",
+					"Month": "$Month",
+					"Day": "$Day",
+					"Hour": "$Hour"
+				}
+			}
+		}, {
+			$group: {
+				_id: {
+					"Year": "$_id.Year",
+					"Month": "$_id.Month",
+					"Day": "$_id.Day",
+					"Hour": "$_id.Hour"
+				},
+				"Total": {
+					$sum: 1
+				}
+			}
+		}, {
+			$sort: {
+				"_id.Year": 1,
+				"_id.Month": 1,
+				"_id.Day": 1,
+				"_id.Hour": 1
+			}
+		}]).toArray();
 
 		return promise.then(function(res) {
 
-			var output=[["Hour", "published"]];
+			var output = [
+				["Hour", "published"]
+			];
 
 			res.forEach(function(e) {
-				var kv=[];
-			 	kv.push(e._id.Year+'/'+e._id.Month+'/'+e._id.Day+' '+e._id.Hour);
+				var kv = [];
+				kv.push(e._id.Year + '/' + e._id.Month + '/' + e._id.Day + ' ' + e._id.Hour);
 				kv.push(e.Total);
-				output.push(kv)
-		 	});
-
-			return output;
-
-	 	});
-
-  }
-
-  _lastMonth(col){
-
-    var promise = col.aggregate([
-      {$match:{"PublishedOn":{$gt:new Date(Date.now() - 30*24*60*60*1000)}}},
-      { "$project": {
-            "EntityId":"$EntityId",
-            "EntityType":"$EntityType",
-            "Year": { $year: "$PublishedOn" },
-            "Month": { $month: "$PublishedOn" },
-            "Day": { $dayOfMonth: "$PublishedOn" }
-       }},
-       {$group:{_id:{"EntityId":"$EntityId","Year":"$Year","Month":"$Month","Day":"$Day"}}},
-       {$group:{_id:{"Year":"$_id.Year","Month":"$_id.Month","Day":"$_id.Day"},"Total":{$sum:1}}},
-       {$sort : { "_id.Year" : 1,"_id.Month" : 1 , "_id.Day" : 1}}
-    ]).toArray();
-
-		return promise.then(function(res) {
-
-			var output=[["Day", "published"]];
-
-			res.forEach(function(e) {
-				var kv=[];
-				kv.push(e._id.Year+'/'+e._id.Month+'/'+e._id.Day);
-				kv.push(e.Total);
-				output.push(kv)
+				output.push(kv);
 			});
 
 			return output;
 
 		});
 
-  }
+	}
 
-  _lastSemester(col){
+	_lastMonth(col) {
 
-    var promise = col.aggregate([
-      {$match:{"PublishedOn":{$gt:new Date(Date.now() - 182*24*60*60*1000)}}}
-      ,{ "$project": {
-            "EntityId":"$EntityId",
-            "EntityType":"$EntityType",
-            "weekStart":{
-              "$subtract": [
-                { "$subtract": [
-                  { "$subtract": [ "$PublishedOn", new Date("1970-01-01") ] },
-                  { "$cond": [
-                    { "$eq": [{ "$dayOfWeek": "$PublishedOn" }, 1 ] },
-                    0,
-                    { "$multiply": [1000 * 60 * 60 * 24,{ "$subtract": [{ "$dayOfWeek": "$PublishedOn" }, 1 ] }]}
-                  ]}
-                ]},
-                { "$mod": [
-                  { "$subtract": [
-                    { "$subtract": [ "$PublishedOn", new Date("1970-01-01") ] },
-                    { "$cond": [
-                      { "$eq": [{ "$dayOfWeek": "$PublishedOn" }, 1 ] },
-                      0,
-                      { "$multiply": [
-                        1000 * 60 * 60 * 24,
-                        { "$subtract": [{ "$dayOfWeek": "$PublishedOn" }, 1 ] }
-                      ]}
-                    ]}
-                  ]},
-                  1000 * 60 * 60 * 24
-                ]}
-              ]
-            },
-            "Year": { $year: "$PublishedOn" },
-            "Month": { $month: "$PublishedOn" },
-            "Week": { $week: "$PublishedOn" }
-       }}
-      ,{$group:{_id:{"EntityId":"$EntityId","Year":"$Year","Month":"$Month","Week":"$Week","weekStart":"$weekStart"}}}
-      ,{$project : {_id : "$_id",weekStart : {$add: [new Date(0), "$_id.weekStart"]}
-              }
-          }
-      ,{$group:{
-          _id:{"Year":"$_id.Year","Month":"$_id.Month","Week":"$_id.Week","weekStart":"$weekStart"},
-          "Total":{$sum:1}
-          }
-        }
-      ,{ $sort : { "_id.Year" : 1,"_id.Month" : 1 , "_id.Week" : 1 } }
-    ]).toArray();
+		var promise = col.aggregate([{
+			$match: {
+				"PublishedOn": {
+					$gt: new Date(Date.now() - 30 * 24 * 60 * 60 * 1000)
+				}
+			}
+		}, {
+			"$project": {
+				"EntityId": "$EntityId",
+				"EntityType": "$EntityType",
+				"Year": {
+					$year: "$PublishedOn"
+				},
+				"Month": {
+					$month: "$PublishedOn"
+				},
+				"Day": {
+					$dayOfMonth: "$PublishedOn"
+				}
+			}
+		}, {
+			$group: {
+				_id: {
+					"EntityId": "$EntityId",
+					"Year": "$Year",
+					"Month": "$Month",
+					"Day": "$Day"
+				}
+			}
+		}, {
+			$group: {
+				_id: {
+					"Year": "$_id.Year",
+					"Month": "$_id.Month",
+					"Day": "$_id.Day"
+				},
+				"Total": {
+					$sum: 1
+				}
+			}
+		}, {
+			$sort: {
+				"_id.Year": 1,
+				"_id.Month": 1,
+				"_id.Day": 1
+			}
+		}]).toArray();
 
 		return promise.then(function(res) {
 
-			var output=[["Week start", "published"]];
+			var output = [
+				["Day", "published"]
+			];
 
 			res.forEach(function(e) {
-				var kv=[];
+				var kv = [];
+				kv.push(e._id.Year + '/' + e._id.Month + '/' + e._id.Day);
+				kv.push(e.Total);
+				output.push(kv);
+			});
+
+			return output;
+
+		});
+
+	}
+
+	_lastSemester(col) {
+
+		var promise = col.aggregate([{
+			$match: {
+				"PublishedOn": {
+					$gt: new Date(Date.now() - 182 * 24 * 60 * 60 * 1000)
+				}
+			}
+		}, {
+			"$project": {
+				"EntityId": "$EntityId",
+				"EntityType": "$EntityType",
+				"weekStart": {
+					"$subtract": [{
+						"$subtract": [{
+							"$subtract": ["$PublishedOn", new Date("1970-01-01")]
+						}, {
+							"$cond": [{
+								"$eq": [{
+									"$dayOfWeek": "$PublishedOn"
+								}, 1]
+							},
+								0, {
+									"$multiply": [1000 * 60 * 60 * 24, {
+										"$subtract": [{
+											"$dayOfWeek": "$PublishedOn"
+										}, 1]
+									}]
+								}
+							]
+						}]
+					}, {
+						"$mod": [{
+							"$subtract": [{
+								"$subtract": ["$PublishedOn", new Date("1970-01-01")]
+							}, {
+								"$cond": [{
+									"$eq": [{
+										"$dayOfWeek": "$PublishedOn"
+									}, 1]
+								},
+									0, {
+										"$multiply": [
+											1000 * 60 * 60 * 24, {
+												"$subtract": [{
+													"$dayOfWeek": "$PublishedOn"
+												}, 1]
+											}
+										]
+									}
+								]
+							}]
+						},
+							1000 * 60 * 60 * 24
+						]
+					}]
+				},
+				"Year": {
+					$year: "$PublishedOn"
+				},
+				"Month": {
+					$month: "$PublishedOn"
+				},
+				"Week": {
+					$week: "$PublishedOn"
+				}
+			}
+		}, {
+			$group: {
+				_id: {
+					"EntityId": "$EntityId",
+					"Year": "$Year",
+					"Month": "$Month",
+					"Week": "$Week",
+					"weekStart": "$weekStart"
+				}
+			}
+		}, {
+			$project: {
+				_id: "$_id",
+				weekStart: {
+					$add: [new Date(0), "$_id.weekStart"]
+				}
+			}
+		}, {
+			$group: {
+				_id: {
+					"Year": "$_id.Year",
+					"Month": "$_id.Month",
+					"Week": "$_id.Week",
+					"weekStart": "$weekStart"
+				},
+				"Total": {
+					$sum: 1
+				}
+			}
+		}, {
+			$sort: {
+				"_id.Year": 1,
+				"_id.Month": 1,
+				"_id.Week": 1
+			}
+		}]).toArray();
+
+		return promise.then(function(res) {
+
+			var output = [
+				["Week start", "published"]
+			];
+
+			res.forEach(function(e) {
+				var kv = [];
 				kv.push(e._id.weekStart);
 				kv.push(e.Total);
-				output.push(kv)
+				output.push(kv);
 			});
 
 			return output;
 
 		});
 
-  }
+	}
 }
